a9232b7809d90f5e0a764af0a31da44e25113959
Dixita Ganatra <dixita0607@gmail.com>
2019-10-07 02:21:05 +0530

    Implement NavBar highlighting of active tab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import {useAuth0} from '../react-auth0-wrapper';
-import {Link} from 'react-router-dom';
+import {Link, useLocation} from 'react-router-dom';
 import {Menu} from 'antd';
 import '../styles/navbar.css';
 
 const NavBar = () => {
   const {isAuthenticated, logout} = useAuth0();
+  const {pathname} = useLocation();
 
   const menuItems = isAuthenticated
     && [
-      <Menu.Item key="users"><Link to="/users">Users</Link></Menu.Item>,
-      <Menu.Item key="profile"><Link to="/profile">Profile</Link></Menu.Item>,
-      <Menu.Item key="logout" onClick={() => logout()}>Log out</Menu.Item>
+      <Menu.Item key="/users"><Link to="/users">Users</Link></Menu.Item>,
+      <Menu.Item key="/profile"><Link to="/profile">Profile</Link></Menu.Item>,
+      <Menu.Item key="/logout" onClick={() => logout()}>Log out</Menu.Item>
     ];
 
   return (
     <div className="nav-bar">
       <div className="app-title">Photo Gallery App</div>
-      <Menu theme="dark" mode="horizontal" style={{lineHeight: '64px'}}>
+      <Menu theme="dark" mode="horizontal" selectedKeys={[pathname]} style={{lineHeight: '64px'}}>
         {menuItems}
       </Menu>
       {isAuthenticated && (
